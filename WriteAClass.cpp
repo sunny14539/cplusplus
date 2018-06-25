353ea397aa29511168f41662ce80097f29fbc7d2
Sunny To <sunny14539@yahoo.com>
2018-06-25 16:25:21 -0700

    Add files via upload

---
+++ WriteAClass.cpp
@@ -0,0 +1,50 @@
+/*
+-NOTE:
+This is fairly terrible code, but is simple to understand
+This is not a representation of great code
+*/
+
+#include <iostream>
+
+class Log 
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo;
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if(m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[WARNING]: " << message << std::endl;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
+	}
+};
+
+int main()
+{
+	Log log;
+	log.SetLevel(log.LogLevelError);
+	log.Warn("Hello!");
+	log.Error("Hello!");
+	log.Info("Hello!");
+	std::cin.get();
+}
